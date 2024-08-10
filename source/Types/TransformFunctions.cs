a83d61764c2aa2cccd41fa37416b048676b35a07
Phill <philldamaskin@gmail.com>
2024-08-10 02:08:54 -0400

    add method for assigning euler angles for rotation

--- source/Types/TransformFunctions.cs
+++ source/Types/TransformFunctions.cs
@@ -1,11 +1,12 @@
 ﻿using Simulation;
+using System;
 using System.Numerics;
 using Transforms;
 using Transforms.Components;
 
 public static class TransformFunctions
 {
-    public static Transform AsTransform<T>(this T entity) where T : IEntity
+    public static Transform BecomeTransform<T>(this T entity) where T : IEntity
     {
         if (!entity.ContainsComponent<T, IsTransform>())
         {
@@ -21,6 +22,11 @@ public static class TransformFunctions
     public static Vector3 GetPosition<T>(this T entity) where T : IPosition
     {
         return entity.GetComponent(new Position()).value;
+    }
+
+    public static ref Vector3 GetPositionRef<T>(this T entity) where T : IPosition
+    {
+        return ref entity.GetComponentRef<T, Position>().value;
     }
 
     /// <summary>
@@ -49,6 +55,11 @@ public static class TransformFunctions
         return entity.GetComponent(new Rotation()).value;
     }
 
+    public static ref Quaternion GetRotationRef<T>(this T entity) where T : IRotation
+    {
+        return ref entity.GetComponentRef<T, Rotation>().value;
+    }
+
     /// <summary>
     /// Assigns the <see cref="Rotation"/> value.
     /// </summary>
@@ -62,12 +73,41 @@ public static class TransformFunctions
         entity.GetComponentRef<T, Rotation>().value = value;
     }
 
+    public static void SetEulerRotation<T>(this T entity, float x, float y, float z) where T : IRotation
+    {
+        float cy = (float)Math.Cos(z * 0.5);
+        float sy = (float)Math.Sin(z * 0.5);
+        float cp = (float)Math.Cos(y * 0.5);
+        float sp = (float)Math.Sin(y * 0.5);
+        float cr = (float)Math.Cos(x * 0.5);
+        float sr = (float)Math.Sin(x * 0.5);
+        Quaternion rotation = new Quaternion
+        {
+            W = cr * cp * cy + sr * sp * sy,
+            X = sr * cp * cy - cr * sp * sy,
+            Y = cr * sp * cy + sr * cp * sy,
+            Z = cr * cp * sy - sr * sp * cy
+        };
+
+        SetRotation(entity, rotation);
+    }
+
+    public static void SetEulerRotation<T>(this T entity, Vector3 eulerAngles) where T : IRotation
+    {
+        SetEulerRotation(entity, eulerAngles.X, eulerAngles.Y, eulerAngles.Z);
+    }
+
     /// <summary>
     /// Retrieves the <see cref="Scale"/> value.
     /// </summary>
     public static Vector3 GetScale<T>(this T entity) where T : IScale
     {
         return entity.GetComponent(new Scale()).value;
+    }
+
+    public static ref Vector3 GetScaleRef<T>(this T entity) where T : IScale
+    {
+        return ref entity.GetComponentRef<T, Scale>().value;
     }
 
     /// <summary>
