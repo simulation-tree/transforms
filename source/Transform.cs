d99d72412b36f2e0542687de30ef246b94db6b15
Phill <philldamaskin@gmail.com>
2024-08-16 23:37:43 -0400

    update

--- source/Transform.cs
+++ source/Transform.cs
@@ -10,6 +10,87 @@ namespace Transforms
     {
         private readonly Entity entity;
 
+        public readonly Vector3 Position
+        {
+            get
+            {
+                if (entity.TryGetComponent(out Position component))
+                {
+                    return component.value;
+                }
+                else
+                {
+                    return Components.Position.Default.value;
+                }
+            }
+            set
+            {
+                ref Position component = ref entity.TryGetComponentRef<Position>(out bool contains);
+                if (contains)
+                {
+                    component.value = value;
+                }
+                else
+                {
+                    entity.AddComponent(new Position(value));
+                }
+            }
+        }
+
+        public readonly Quaternion Rotation
+        {
+            get
+            {
+                if (entity.TryGetComponent(out Rotation component))
+                {
+                    return component.value;
+                }
+                else
+                {
+                    return Components.Rotation.Default.value;
+                }
+            }
+            set
+            {
+                ref Rotation component = ref entity.TryGetComponentRef<Rotation>(out bool contains);
+                if (contains)
+                {
+                    component.value = value;
+                }
+                else
+                {
+                    entity.AddComponent(new Rotation(value));
+                }
+            }
+        }
+
+        public readonly Vector3 Scale
+        {
+            get
+            {
+                if (entity.TryGetComponent(out Scale component))
+                {
+                    return component.value;
+                }
+                else
+                {
+                    return Components.Scale.Default.value;
+                }
+            }
+            set
+            {
+                ref Scale component = ref entity.TryGetComponentRef<Scale>(out bool contains);
+                if (contains)
+                {
+                    component.value = value;
+                }
+                else
+                {
+                    entity.AddComponent(new Scale(value));
+                }
+            }
+        }
+
         World IEntity.World => entity.world;
         eint IEntity.Value => entity.value;
 
