5b32d02dea5240adebdda62977802e6804bd15fb
Phill <philldamaskin@gmail.com>
2024-09-25 19:13:40 -0400

    more pivot defaults

--- source/Components/Pivot.cs
+++ source/Components/Pivot.cs
@@ -1,17 +1,18 @@
-﻿#pragma warning disable CS8981
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace Transforms.Components
 {
     public struct Pivot
     {
         public static readonly Vector3 Centered = new(0.5f, 0.5f, 0.5f);
+        public static readonly Vector3 BottomRight = new(1f, 0f, 0f);
         public static readonly Vector3 BottomLeft = new(0f, 0f, 0f);
         public static readonly Vector3 TopRight = new(1f, 1f, 0f);
+        public static readonly Vector3 TopLeft = new(0f, 1f, 0f);
 
         public Vector3 value;
 
-        public Pivot(float x, float y, float z)
+        public Pivot(float x, float y, float z = default)
         {
             value = new Vector3(x, y, z);
         }
