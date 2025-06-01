ce1074a87734d4c22ab361fd0c293e736d984da8
Phill <philldamaskin@gmail.com>
2025-06-01 12:25:02 -0400

    add update message

---
+++ source/Messages/TransformUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace Transforms.Messages
+{
+    /// <summary>
+    /// Update message to update transform components and entities.
+    /// </summary>
+    public readonly struct TransformUpdate
+    {
+    }
+}
